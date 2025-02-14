a13360dddae342486f1cb57bfbb3336514c13ebd
JeonB <newjeonb@gmail.com>
2025-02-14 22:57:46 +0900

    테일윈드, 코드포맷팅 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  compress: true,
+  serverRuntimeConfig: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+  devIndicators: {
+    appIsrStatus: false,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
